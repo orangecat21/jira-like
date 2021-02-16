d3580dc5763c2a5ef1ad1070d468724e3f094b48
orangefruit21 <orangecat21@yandex.ru>
2021-02-17 02:00:38 +0300

    feat: action creators was created

---
+++ src/redux/reducers/actionCreators.js
@@ -0,0 +1,36 @@
+import actionTypes from '../actionTypes';
+
+export const addTaskAC = (task) => {
+    return {
+        type: actionTypes.ADD_TASK,
+        task,
+    };
+};
+
+export const moveToOrderAC = (task) => {
+    return {
+        type: actionTypes.MOVE_TASK_TO_ORDER,
+        task
+    }
+};
+
+export const moveToWorkAC = (task) => {
+    return {
+        type: actionTypes.MOVE_TASK_TO_WORK,
+        task
+    }
+};
+
+export const moveToInspectionAC = (task) => {
+    return {
+        type: actionTypes.MOVE_TASK_TO_INSPECTION,
+        task
+    }
+};
+
+export const moveToCompleteAC = (task) => {
+    return {
+        type: actionTypes.MOVE_TASK_TO_ORDER,
+        task
+    }
+};
